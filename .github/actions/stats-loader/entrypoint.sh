d29c0e233471a8a9c62e05f949412aefd9a01e5f
Benjamin Lannon <benjamin@lannonbr.com>
2019-09-04 20:39:55 -0400

    Try fixing update workflow

--- .github/actions/stats-loader/entrypoint.sh
+++ .github/actions/stats-loader/entrypoint.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 
+pwd
+
 cd $GITHUB_WORKSPACE
 
-node /load-data.js > src/data/data.json
+# node /load-data.js > src/data/data.json
 
 git config user.name "GitHub Action Bot"
 git config user.email "<>"
 
 git remote set-url origin https://x-access-token:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git
 
-git checkout master
+git remote -v
 
-git add src/data/data.json
+git branch
 
-git commit -m "Updating data.json"
-git push -u origin master
+# git checkout master
+
+# git add src/data/data.json
+
+# git commit -m "Updating data.json"
+# git push -u origin master
