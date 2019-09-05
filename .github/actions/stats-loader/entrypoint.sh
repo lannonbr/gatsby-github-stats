22f3592d0c46f0468f21d84f1ab8e18d3ac88f10
Benjamin Lannon <benjamin@lannonbr.com>
2019-09-04 22:10:04 -0400

    Final test for the night

--- .github/actions/stats-loader/entrypoint.sh
+++ .github/actions/stats-loader/entrypoint.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-node /load-data.js > src/data/data.json
-
 git config user.name "GitHub Action Bot"
 git config user.email "<>"
 
@@ -9,7 +7,9 @@ git checkout -B master origin/master
 
 git remote set-url origin https://x-access-token:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git
 
+node /load-data.js > src/data/data.json
+
 git add src/data/data.json
 
 git commit -m "Updating data.json"
-git push origin master
+git push origin HEAD:master
