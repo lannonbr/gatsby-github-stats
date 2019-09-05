0ed8c3c632d0a525ad944a23c98acc519c47d95d
Benjamin Lannon <benjamin@lannonbr.com>
2019-09-04 21:18:10 -0400

    continual work with action

--- .github/actions/stats-loader/entrypoint.sh
+++ .github/actions/stats-loader/entrypoint.sh
@@ -5,6 +5,8 @@ node /load-data.js > src/data/data.json
 git config user.name "GitHub Action Bot"
 git config user.email "<>"
 
+git checkout -B master origin/master
+
 git remote set-url origin https://x-access-token:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git
 
 git add src/data/data.json
