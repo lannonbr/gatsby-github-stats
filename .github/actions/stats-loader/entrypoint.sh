d9adb16154e052bb75342000ac36fb1bc8898083
Benjamin Lannon <benjamin@lannonbr.com>
2019-09-04 20:47:05 -0400

    Update action & workflow

--- .github/actions/stats-loader/entrypoint.sh
+++ .github/actions/stats-loader/entrypoint.sh
@@ -1,23 +1,15 @@
 #!/bin/bash
 
-pwd
-
-cd $GITHUB_WORKSPACE
-
-# node /load-data.js > src/data/data.json
+node /load-data.js > src/data/data.json
 
 git config user.name "GitHub Action Bot"
 git config user.email "<>"
 
 git remote set-url origin https://x-access-token:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git
 
-git remote -v
+git checkout $REF
 
-git branch
+git add src/data/data.json
 
-# git checkout master
-
-# git add src/data/data.json
-
-# git commit -m "Updating data.json"
-# git push -u origin master
+git commit -m "Updating data.json"
+git push -u origin "${REF}"
