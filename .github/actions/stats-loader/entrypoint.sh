d8aaf0c7a4e9cd1a879376754c0bd2c6ebf45ccb
Benjamin Lannon <benjamin@lannonbr.com>
2019-09-04 20:52:00 -0400

    Update action

--- .github/actions/stats-loader/entrypoint.sh
+++ .github/actions/stats-loader/entrypoint.sh
@@ -7,9 +7,7 @@ git config user.email "<>"
 
 git remote set-url origin https://x-access-token:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git
 
-git checkout $REF
-
 git add src/data/data.json
 
 git commit -m "Updating data.json"
-git push -u origin "${REF}"
+git push origin master
